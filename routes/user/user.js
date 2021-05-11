74d78898b39607c54c19287ba67c7809aeaff8c2
Jade Cohen <jadecohen97@gmail.com>
2021-05-11 11:56:00 +0300

    connecting db check

--- routes/user/user.js
+++ routes/user/user.js
@@ -1,31 +1,65 @@
-const express = require('express');
-const bcrypt = require('bcrypt');
+const express = require("express");
+const bcrypt = require("bcrypt");
 // const { upload } = require('../middlewares/imageUpload'); // for img
 // const { uploadToCloudinary } = require('../lib/cloudinary'); // for img
 // const fs = require('fs'); // for img
-const jwt = require('jsonwebtoken'); 
-// const { auth } = require('../middlewares/auth'); authentication middle
+const jwt = require("jsonwebtoken");
+
+const { users_collection } = require("../../database");
+// const { MongoClient, ObjectID } = require("mongodb");
+// const { auth } = require('../middlewares/auth'); autheçntication middle
 // const { checkIfAdmin } = require('../middlewares/checkAdmin'); // admin middle
-// const validationMiddleware = require('../middlewares/validation'); 
-// 
+// const validationMiddleware = require('../middlewares/validation');
+// const url =
+//   "mongodb+srv://dbUser:user000@cluster0.1t5ad.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+// const client = new MongoClient(url, { useUnifiedTopology: true });
+
+// const dbName = "ourDb";
+// let users_collection = "";
+
+// async function run() {
+//     try {
+//       await client.connect();
+//       const db = client.db(dbName);
+//       users_collection = db.collection("Users");
+//     } catch (err) {
+//       console.log(err.stack);
+//   }
+// }
+//   run().catch(console.dir);
 
 const router = express.Router();
 
 //mock data
+
+const getAllUsers = async () => {
+  const all_db_users = await users_collection.find().toArray();
+  return all_db_users;
+};
+
 const mockUser = {
-    firstName: "baruch",
-    lastName: "baruch",
-    email: "baruch@baruch.com",
-    password_hash: "$2b$10$WWtl5qLj6A2bDAe.XpiqCeEbZ03wgr0L6.ayhxE/sDJHrAnrwrLum",
-}
+  firstName: "baruch",
+  lastName: "baruch",
+  email: "baruch@baruch.com",
+  password_hash: "$2b$10$WWtl5qLj6A2bDAe.XpiqCeEbZ03wgr0L6.ayhxE/sDJHrAnrwrLum",
+};
 
+router.post("/user/chat", async (req, res, next) => {
+  const { body } = req.body;
+  res.status(200);
+});
 
-router.post('/user/chat',
-    async (req, res, next) => {
-        
-        
-        const {body} = req.body;
-        res.status(200)
-    })
+router.get("/users", (req, res) => {
+  try {
+    // Get all users from Mongo
+    allUsers = getAllUsers().then((users) => {
+      res.send(users);
+    });
+  } catch (err) {
+    res.send(
+      `We have error: ${err.stack}. Sorry. We appreciate your patience while we work this out.`
+    );
+  }
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,14 +1,49 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+let path = require("path");
+
+
+const { MongoClient, ObjectID } = require("mongodb");
+const url =
+  "mongodb+srv://dbUser:user000@cluster0.1t5ad.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(url, { useUnifiedTopology: true });
+
+const dbName = "ourDb";
+let users_collection = "";
+let db
+const run = async () => {
+  try {
+    await client.connect();
+     db = client.db(dbName);
+    // return db;
+    // users_collection = db.collection("Users");
+  } catch (err) {
+    console.log(err.stack);
+  }
+}
+run().catch(console.dir);
+db.collection()
+
+
 
 
 const app = express();
 app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname)));
+app.use("/styles", express.static(__dirname));
 
 app.use(cors());
-app.use('/signUp', require('./routes/signUp/signUp'));
-app.use('/logIn', require('./routes/logIn/logIn'));
-app.use('/user', require('./routes/user/user'));
+app.use("/signUp", require("./routes/signUp/signUp"));
+app.use("/logIn", require("./routes/logIn/logIn"));
+app.use("/user", require("./routes/user/user"));
+
+
+
 
 
 
@@ -17,5 +52,5 @@ const port = 6000;
 const host = "127.0.0.1";
 
 app.listen(port, host, () => {
-    console.log(`the server is listening at http://${host}:${port}`);
+  console.log(`the server is listening at http://${host}:${port}`);
 });
