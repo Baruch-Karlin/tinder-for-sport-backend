1f78ca4081c3d970888c8cd57959705c24cf7356
Baruch Karlin <76958079+Baruch-Karlin@users.noreply.github.com>
2021-05-13 12:12:33 +0300

    json chat schema added

--- routes/chat/chat.js
+++ routes/chat/chat.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const { auth } = require('../../middlewares/auth');
-// const postValidationMiddleware = require('../../middlewares/postValidation');
-// const chatValidationMiddleware = require('../../middlewares/cahtValidation');
-// const { postValidateSchema } = require('./postSchema');
-// const { chatValidateSchema } = require('./chatSchema');
+const postValidationMiddleware = require('../../middlewares/postValidation');
+const { postValidateSchema } = require('./postSchema');
 
 const mongoose = require('mongoose');
 const Chat = require('./mongoose_modle.js/chat');
 const User = require('../user/mongoose_modle/user');
 
 const router = express.Router();
+
 
 router.post(
     '/createChat/:userId',
@@ -22,7 +21,6 @@ router.post(
         const accepterId = req.user.uid;
         console.log(senderId);
         console.log(accepterId);
-
 
         const chat = new Chat({
             _id: new mongoose.Types.ObjectId(),
@@ -38,6 +36,7 @@ router.post(
 
 router.post(
     '/createPost/:userId/:chatId',
+    postValidationMiddleware(postValidateSchema),
     auth,
     async (req, res, next) => {
         //finds the chat by id- params
@@ -61,64 +60,5 @@ router.post(
         const createdPost = await chatToAddPost.save();
         res.status(200).send(createdPost)
     })
-
-
-
-// ///////////////////    chat api - to be removed from here!!11/////
-// // not good- need to be able to add multiple chats
-// router.post('/chat/:userId',
-//     auth,
-//     chatValidationMiddleware(chatValidateSchema),
-//     //same user
-//     //check if exists posts[0] if yes return
-//     async (req, res, next) => {
-//         const id = req.params.userId;
-//         const newChat = req.body;
-//         console.log('2', newChat.chat[0].posts[0]);
-//         const result = await User.updateOne({ _id: id }, {
-//             $set: {
-//                 chat:
-//                     [{
-//                         _id: new mongoose.Types.ObjectId(),
-//                         posts: [{
-//                             _id: new mongoose.Types.ObjectId(),
-//                             name: newChat.chat[0].posts[0].name,
-//                             body: newChat.chat[0].posts[0].body
-//                         }]
-//                     }]
-//             }
-//         });
-//         // should also update reciving user
-//         res.status(200).send(newChat.chat[0].posts[0]);
-//     })
-
-// router.post('/chat/:userId/:chatId',
-//     //auth
-//     // postValidationMiddleware(postValidateSchema),
-//     //same user
-//     async (req, res, next) => {
-//         // instead of find query db
-//         const id = req.params.userId;
-//         const chatId = req.params.chatId
-//         const post = req.body
-//         const updatedUser = await User.findById(id);
-//         // console.log(updatedUser);
-//         // console.log(post)
-//         // need to find index of chat
-//         const arr = updatedUser.chat[0].posts.push(post);
-//         await updatedUser.save()
-//         res.status(200).send(updatedUser.chat[0].posts)
-
-
-//         // const foundChat = mockChats.find((chat) => {
-//         //     if (chat.id == req.params.chatId)
-//         //         return (chat);
-//         // })
-//         // //instead of push add
-//         // console.log(post)
-//         // //create an id + date in the db
-//         // foundChat.posts.push(post)
-//         // res.status(200).send(foundChat);
-//     })
 
 module.exports = router;
