d244735b6946a528320868fb00342ff5d7dd302b
Yossef Vashdi <76695165+yossef-vashdi@users.noreply.github.com>
2021-05-19 07:11:19 +0300

    nothing

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,25 +1,20 @@
-
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 function auth(req, res, next) {
-    const { authorization } = req.headers;
-    if (!authorization) {
-        res.status(401).send({ message: 'Must provide an authorization' });
-        return;
+  const { authorization } = req.headers;
+  if (!authorization) {
+    res.status(401).send({ message: "Must provide an authorization" });
+    return;
+  }
+  const token = authorization.replace("Bearer ", "");
+  jwt.verify(token, "sfdsf5sfs64s65f4sdfsdf", async (err, decoded) => {
+    if (err) {
+      res.status(401).send({ message: "Invalid token" });
+      return;
     }
-    const token = authorization.replace('Bearer ', '');
-    jwt.verify(token, 'sfdsf5sfs64s65f4sdfsdf', async (err, decoded) => {
-        if (err) {
-            res.status(401).send({ message: 'Invalid token' });
-            return;
-        }
-        req.user = decoded;
-        next();
-    });
+    req.user = decoded;
+    next();
+  });
 }
 
 exports.auth = auth;
-
-
-
-
