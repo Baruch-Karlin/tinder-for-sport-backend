f38a1318f0d26d63e1ee21a2ea0c027416f85344
Yossef Vashdi <76695165+yossef-vashdi@users.noreply.github.com>
2021-05-19 21:54:41 +0300

    insignificant

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,12 +6,7 @@ function auth(req, res, next) {
     res.status(401).send({ message: "Must provide an authorization" });
     return;
   }
-  // const token = authorization.replace("Bearer ", "");
-  // jwt.verify(token, "sfdsf5sfs64s65f4sdfsdf", async (err, decoded) => {
-  //   if (err) {
-  //     res.status(401).send({ message: "Invalid token" });
-  //     return;
-  //   }
+
   const token = authorization.replace("Bearer ", "");
   jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
     if (err) {
